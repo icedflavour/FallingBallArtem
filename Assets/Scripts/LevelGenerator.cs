5b9d6d271401e2c25c93ac9defe5a2bb576f660d
f1avour <iced.flavour@gmail.com>
2024-10-26 11:10:43 +0300

    Better Generation with Floors

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,24 +4,37 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject Level;
     [SerializeField] private GameObject Column;
     [SerializeField] private GameObject Floor;
-    [SerializeField] private GameObject FloorSegment;
-    [SerializeField] private int SegmentsQuantity;
+    [SerializeField] private GameObject BasicSegment;
+    [SerializeField] private GameObject KillSegment;
+    [SerializeField] private GameObject GlassSegment;
+
+    [SerializeField] private int FloorsNumber;
+    [SerializeField] private int FloorsGap;
+    [SerializeField] private int SegmentsNumber;
     [SerializeField] private int AngleDiff;
 
     private void Start()
     {
-        Instantiate(Column);
-        var parentFloorInstance = Instantiate(Floor);
+        var LevelInstance = Instantiate(Level);
 
-        var spawnPosition = new Vector3(0.0f, 0.0f, 0.0f);
+        var ColumnInstance = Instantiate(Column, LevelInstance.transform);
 
-        for (int i = 0; i < 24; i++)
+        ColumnInstance.transform.position = new Vector3(0f, FloorsGap * (FloorsNumber + 2) * 0.25f, 0f);
+        ColumnInstance.transform.localScale = new Vector3(2f, FloorsGap * (FloorsNumber + 2) * 0.25f, 2f);
+
+        for (int i = 0; i < FloorsNumber; i++)
         {
-            var floorSegmentInstance = Instantiate(FloorSegment, spawnPosition, Quaternion.identity, parentFloorInstance.transform);
-            floorSegmentInstance.transform.Rotate(0.0f, AngleDiff * i, 0.0f, Space.Self);
+            var floorSpawnPosition = new Vector3(0.0f, i * FloorsGap, 0.0f);
+            var FloorInstance = Instantiate(Floor, floorSpawnPosition, Quaternion.identity, LevelInstance.transform);
+
+            for (int j = 0; j < SegmentsNumber; j++)
+            {
+                var floorSegmentInstance = Instantiate(BasicSegment, floorSpawnPosition, Quaternion.identity, FloorInstance.transform);
+                floorSegmentInstance.transform.Rotate(0.0f, AngleDiff * j, 0.0f, Space.Self);
+            }
         }
     }
 }
