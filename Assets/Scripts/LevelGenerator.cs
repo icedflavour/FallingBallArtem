9d1669880a7d2b272c5ed531a139b01b108c2824
f1avour <iced.flavour@gmail.com>
2025-01-11 10:22:21 +0200

    Updated Kill Blocks

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,7 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
@@ -17,6 +16,8 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private int FloorsGap;
     [SerializeField] private int SegmentsNumber;
     [SerializeField] private int AngleDiff;
+    [SerializeField] private int GapDifficulty;
+    [SerializeField] private int KillDifficulty;
 
     private void Start()
     {
@@ -25,24 +26,40 @@ public class LevelGenerator : MonoBehaviour
 
     private void GenerateLevel()
     {
-        var LevelInstance = Instantiate(Level);
+        var levelInstance = Instantiate(Level);
 
-        var ColumnInstance = Instantiate(Column, LevelInstance.transform);
-        ColumnInstance.transform.position = new Vector3(0f, FloorsGap * FloorsNumber * 0.5f, 0f);
-        ColumnInstance.transform.localScale = new Vector3(2f, FloorsGap * FloorsNumber * 0.5f, 2f);
+        var columnInstance = Instantiate(Column, levelInstance.transform);
+        columnInstance.transform.position = new Vector3(0f, FloorsGap * FloorsNumber * 0.5f, 0f);
+        columnInstance.transform.localScale = new Vector3(2f, FloorsGap * FloorsNumber * 0.5f, 2f);
 
-        GenerateFloors(LevelInstance);
+        GenerateFloors(levelInstance);
     }
+
 
     private void GenerateFloors(GameObject levelObject)
     {
         for (int i = 0; i < FloorsNumber; i++)
         {
             var floorSpawnPosition = new Vector3(0.0f, i * FloorsGap, 0.0f);
-            var FloorInstance = Instantiate(Floor, floorSpawnPosition, Quaternion.identity, levelObject.transform);
+            var floorInstance = Instantiate(Floor, floorSpawnPosition, Quaternion.identity, levelObject.transform);
 
-            var gapBasis = Random.Range(0, SegmentsNumber);
-            int[] gapIndexes = new int[5]; // switch to level difficulty
+            var gapBasis = Random.Range(0, SegmentsNumber); // switch to level difficulty
+            int[] gapIndexes = new int[GapDifficulty];
+            int[] killIndexes = new int[KillDifficulty];
+            var killBegin = (gapBasis + GapDifficulty) % SegmentsNumber;
+
+            // TASK 3
+            for (int j = 0; j < KillDifficulty; j++)
+            {
+                var killIndex = (killBegin + Random.Range(0, SegmentsNumber - GapDifficulty)) % SegmentsNumber;
+
+                while (killIndexes.Contains(killIndex))
+                {
+                    killIndex = (killBegin + Random.Range(0, SegmentsNumber - GapDifficulty)) % SegmentsNumber;
+                }
+
+                killIndexes[j] = killIndex;
+            }
 
             for (int index = 0; index < gapIndexes.Length; index++)
             {
@@ -54,14 +71,20 @@ public class LevelGenerator : MonoBehaviour
                 if (gapIndexes.Contains(j))
                 {
                     continue;
-                } 
+                }
+                else if (killIndexes.Contains(j))
+                {
+                    var KillSegmentInstance = Instantiate(KillSegment, floorSpawnPosition, Quaternion.identity, floorInstance.transform);
+                    KillSegmentInstance.transform.Rotate(0.0f, AngleDiff * j, 0.0f, Space.Self);
+                    KillSegmentInstance.GetComponent<Segment>().Index = j; ///
+                }
                 else
                 {
-                    var floorSegmentInstance = Instantiate(BasicSegment, floorSpawnPosition, Quaternion.identity, FloorInstance.transform);
+                    var floorSegmentInstance = Instantiate(BasicSegment, floorSpawnPosition, Quaternion.identity, floorInstance.transform);
                     floorSegmentInstance.transform.Rotate(0.0f, AngleDiff * j, 0.0f, Space.Self);
+                    floorSegmentInstance.GetComponent<Segment>().Index = j; ///
                 }
             }
         }
     }
 }
-
